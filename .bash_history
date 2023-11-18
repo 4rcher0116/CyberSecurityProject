b8c8b882c38f8460f49369b5e4eea84a451f5bfa
Andrew N <4rcher0116@gmail.com>
2023-11-18 19:44:50 +0000

    Initial Commit

---
+++ .bash_history
@@ -0,0 +1,27 @@
+sudo apt install apache2
+sudo apt install sqlite3
+sqlite3 filemanage.db
+sudo apt install nodejs
+ls
+pwd
+ls
+mkdir userfiles
+ls
+node -v
+mkdir file-management-backend
+ls
+cd file-management-backend/
+npm init -y
+cd ..
+sudo apt install npm
+ls
+cd file-management-backend/
+npm init -y
+ls
+npm install express sqlite3 --save
+ls
+vim app.js
+LS
+npm install multer fs --save
+ls
+vim app.js
---
+++ file-management-backend/app.js
@@ -0,0 +1,79 @@
+
+//Required Dependencies
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose(); //import SQLite3 Module
+
+//Dependencies for File upload
+const multer = require('multer');
+const fs = require('fs');
+
+//Configure Server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Establish Connection to SQLite Database
+const db = new sqlite3.Database('../filemanage.db');
+
+//Multer configuration to handle file uploads
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+	//Create directory for user if it does not exist
+		const userId = req.params.userId;
+		const userDirectory = `../userfiles/${userId}`;
+		if(!fs.existsSync(userDirectory)) {
+			fs.mkdirSync(userDirectory, { recursive: true });
+		}
+		cb(null, userDirectory);
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	}
+
+});
+
+const upload = multer({ storage: storage });
+
+//Route for handling file upload
+app.post('/upload/:userId', upload.single('file'), (req, res) => {
+	const userId = req.params.userId;
+	const filePath = req.file.path;
+
+	//Insert file path inot the database
+	db.run('INSERT INTO files (user_id, filename, file_path) VALUES (?, ?, ?)', [userId, req.file.originalname, filePath], (err) => {
+		if (err){
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.status(200).send('File uploaded and path was stored in Database');
+	});
+});
+
+//Route for retreiving files list of user
+app.get('../userfiles/:userId', (req, res) => {
+	const userId = req.params.userId;
+
+	//query files associated with user from database
+	db.all('SELECT * FROM files WHERE user_id = ?', [userId], (err, rows) => {
+		if(err){
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.json(rows)
+	});
+
+});
+
+//Route for deleting a file 
+
+
+
+
+
+
+
+
+
+//Start up the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
